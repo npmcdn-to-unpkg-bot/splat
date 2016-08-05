4396d74dc700b40002189d3442b7a68b41947a1a
Jeff G <jg@okayjeff.com>
2016-08-05 08:28:36 -0400

    check if params are present before attempting to geocode a location on initialization

--- models/location.rb
+++ models/location.rb
@@ -3,13 +3,14 @@ class Location
                 :owner_verfied, :spv2
 
   def initialize(args = {})
+    return nil if args.empty?
     @name          = args[:name]
     @address       = args[:address]
     @city          = args[:city]
     @state         = args[:state]
     @zip           = args[:zip]
     @owner_verfied = false
-    geocode
+    geocode unless unable_to_geocode?(args)
   end
 
   def geocode
@@ -21,5 +22,23 @@ class Location
                                         ")
     @lat = coords[0]
     @lng = coords[1]
+  end
+
+  private
+
+  def unable_to_geocode?(args)
+    address_missing?(args) || city_missing?(args) || state_missing?(args)
+  end
+
+  def address_missing?(args)
+    args[:address].nil? || args[:address].empty?
+  end
+
+  def city_missing?(args)
+    args[:city].nil? || args[:city].empty?
+  end
+
+  def state_missing?(args)
+    args[:state].nil? || args[:state].empty?
   end
 end
