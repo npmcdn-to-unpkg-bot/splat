958e9641651920f8ad2e020b1406bde23155bb1b
Jeff Gharakhanian <jg@okayjeff.com>
2016-08-18 11:59:02 -0400

    Deprecate validation methods to check if location address components are present

--- models/location.rb
+++ models/location.rb
@@ -25,22 +25,4 @@ class Location < ActiveRecord::Base
   def owner_verified?
     self.parent_business_id != nil
   end
-
-  private
-
-  def unable_to_geocode?(args)
-    address_missing?(args) || city_missing?(args) || state_missing?(args)
-  end
-
-  def address_missing?(args)
-    args[:address_1].nil? || args[:address_1].empty?
-  end
-
-  def city_missing?(args)
-    args[:city].nil? || args[:city].empty?
-  end
-
-  def state_missing?(args)
-    args[:region_id].nil? || args[:region_id].empty?
-  end
 end
